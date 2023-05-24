bb4b084135ea7fb0b64c0cf3b0a17a3d6bb84565
kunalramteke5 <95569590+kunalramteke5@users.noreply.github.com>
2023-05-24 22:12:14 +0530

    assinment 1 and 2 java

---
+++ assig124.java
@@ -0,0 +1,16 @@
+public class assig1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        boolean var1 = num1 == num2;
+        boolean var2 = num1 != num2;
+        boolean var3 = num1 > num2;
+        boolean var4 = num1 < num2;
+
+        System.out.println("Equal: " + var1);
+        System.out.println("Not Equal: " + var2);
+        System.out.println("num1 is Greater Than num2: " + var3);
+        System.out.println("num1 is Less Than num2: " + var4);
+
+    }
+}
